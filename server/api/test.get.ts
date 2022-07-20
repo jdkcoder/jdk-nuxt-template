51cdbe1ed2e3550af04425cec7cb8d2061aeb8f2
Vũ Quang Minh <jokerdarkknight@protonmail.com>
2022-07-21 03:10:27 +0700

    update

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 export default defineEventHandler(async () => {
-  const body = "x5ihf9FgkOig5CNdNEbVWdEJV";
   let result;
-  let test = await axios
-    .get(`https://v2.zeelios.vip/api/keys/show/${body}`)
-    .then((res) => (result = res.data));
+  let test = await axios.get(`url`).then((res) => (result = res.data));
 
   return { result };
 });
